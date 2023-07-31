2530c428f5da42212db0d78e1a3492f12ae535d9
Denver04 <=>
2023-07-31 19:50:24 +0530

    WIP: 18551bc first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import React, { useContext } from "react";
 // import utamhime from "../../public/Images/utahime.png";
 import style from "./CSS/navbar.module.css";
@@ -10,8 +9,8 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { AiFillHome } from "react-icons/ai";
 import Theme from "./Context/Theme";
 
-
 export const Navbar = () => {
+
   const { mode, toggleMode } = useContext(Theme);
   const handleDark = () => {
     toggleMode("light" ? "dark" : "light");
@@ -20,12 +19,12 @@ export const Navbar = () => {
   return (
     <nav className={style.nav} theme={mode}>
       <div className={style.nav_head}>
-        <div className={style.image_div}>
+        <Link style={{borderRadius:"50%"}} href="/"><div className={style.image_div}>
           <h1>A</h1>
-        </div>
-        <div className={style.nav_header}>
+        </div></Link>
+        <Link style={{backgroundColor:"inherit" , textDecoration:"none"}} href="/"><div className={style.nav_header}>
           <h2>Instagram</h2>
-        </div>
+        </div></Link>
       </div>
       <div className={style.nav_link}>
         <Link className={style.mode_icon} href="/">
